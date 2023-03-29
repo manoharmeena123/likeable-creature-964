06f764cd198fc8fc14d975fc98d64e79fbf52243
krishna <manoharmeena242@gmail.com>
2023-03-29 22:42:56 +0530

    Day-2

--- Backend/service/redis.js
+++ Backend/service/redis.js
@@ -9,18 +9,18 @@
 //     password:  process.env.pass,
 //     db: 0, // Defaults to 0
 //   });
-const redis = require("redis");
-const client = redis.createClient({
-  password: 'VXa1WN4beqRBSXvQnxH68cQrQmbr5Bhd',
-  socket: {
-      host: 'redis-10196.c305.ap-south-1-1.ec2.cloud.redislabs.com',
-      port: 10196
-  }
-});
-client.on('error', err => console.log('Redis Client Error', err));
-client.connect();
+// const redis = require("redis");
+// const client = redis.createClient({
+//   password: 'VXa1WN4beqRBSXvQnxH68cQrQmbr5Bhd',
+//   socket: {
+//       host: 'redis-10196.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+//       port: 10196
+//   }
+// });
+// client.on('error', err => console.log('Redis Client Error', err));
+// client.connect();
 
 
-module.exports={
-  client
-}     
+// module.exports={
+//   client
+// }     
